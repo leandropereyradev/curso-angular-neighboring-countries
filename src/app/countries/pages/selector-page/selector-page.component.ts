c531bed6924d76e8557278a84169bd5dc2ae3d2e
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-29 09:23:09 +0200

    First commit: folders and SelectorComponent

---
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'countries-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [],
+})
+export class SelectorPageComponent {}
