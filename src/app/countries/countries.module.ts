19fc72bb3dfed743b5e91fc12cb462f576aafe30
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-29 09:50:35 +0200

    SelectorPage: Connecting Reactive Form with HTML

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { CountriesRoutingModule } from './contries-rounting.module';
 
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 
 @NgModule({
   declarations: [SelectorPageComponent],
-  imports: [CommonModule, CountriesRoutingModule],
+  imports: [CommonModule, CountriesRoutingModule, ReactiveFormsModule],
 })
 export class CountriesModule {}
